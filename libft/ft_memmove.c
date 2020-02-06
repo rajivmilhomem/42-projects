e40b01cbad25cbe03788ee0dd56a937f7cd420f6
rajivmilhomem <rajivmilhomem@gmail.com>
2020-02-06 20:21:56 -0300

    Subir

---
+++ libft/ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtavares <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 18:49:16 by rtavares          #+#    #+#             */
+/*   Updated: 2020/02/22 19:20:00 by rtavares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*dest;
+	unsigned char	*orig;
+	size_t			i;
+
+	dest = (unsigned char *)dst;
+	orig = (unsigned char *)src;
+	i = 0;
+	if (dest > orig)
+	{
+		while (len--)
+		{
+			dest[len] = orig[len];
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			dest[len] = orig[len];
+			i++;
+		}
+	}
+	return (dst);
+}
